9f5eb621e644905d61f86abd57eecb9814637e77
Rey Marquez <reymarquez@calebs-macbook.attlocal.net>
2017-06-15 15:30:37 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 console.log("Up and running!")
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!")
+	} else {
+		alert("Sorry, try again.")
+	}
+}
+console.log()
 
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardThree);
